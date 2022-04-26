e0ffd14ea1d6e96c86ccfade71f963394d8be5c8
sylleryum <alisson_piuco@yahoo.com.br>
2022-04-26 18:53:37 -0300

    test cases for payment service

--- payment/src/test/java/com/sylleryum/payment/service/PaymentServiceTest.java
+++ payment/src/test/java/com/sylleryum/payment/service/PaymentServiceTest.java
@@ -3,10 +3,12 @@ package com.sylleryum.payment.service;
 import com.sylleryum.common.config.GlobalConfigs;
 import com.sylleryum.common.entity.Item;
 import com.sylleryum.common.entity.Order;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
 import java.util.List;
@@ -14,25 +16,26 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
+@SpringBootTest
 class PaymentServiceTest {
 
     @Autowired
     PaymentService paymentService;
     @MockBean
     GlobalConfigs globalConfigs;
-    Order orderTest;
+    static Order orderTest;
 
-    @BeforeEach
-    void setUp() {
-        Item item = new Item(globalConfigs.itemPrefix() + 9, 5);
-        Item item1 = new Item(globalConfigs.itemPrefix() + 5, 3);
-        Item item2 = new Item(globalConfigs.itemPrefix() + 2, 8);
+    @BeforeAll
+    static void beforeAll() {
+        Item item = new Item("test" + 9, 5);
+        Item item1 = new Item("test" + 5, 3);
+        Item item2 = new Item("test" + 2, 8);
         orderTest = new Order(
                 "orderNumber", List.of(item, item1, item2), 3L);
     }
 
     @Test
-    void newPayment_success_true() {
+    void newPayment_successPayment_true() {
         Mockito.when(globalConfigs.statusPayment()).thenReturn(1);
         boolean isPaymentCompleted = paymentService.newPayment(orderTest);
 
@@ -40,7 +43,7 @@ class PaymentServiceTest {
     }
 
     @Test
-    void newPayment_failure_true() {
+    void newPayment_successPayment_False() {
         Mockito.when(globalConfigs.statusPayment()).thenReturn(-1);
         boolean isPaymentCompleted = paymentService.newPayment(orderTest);
 
@@ -49,6 +52,8 @@ class PaymentServiceTest {
 
     @Test
     void rollbackPayment() {
+        Mockito.when(globalConfigs.statusPayment()).thenReturn(1);
+
         boolean rollbackPaymentResult = paymentService.rollbackPayment(orderTest);
         assertThat(rollbackPaymentResult).isTrue();
     }
